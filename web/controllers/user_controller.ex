5eb26b73f8c17dd0a1fa65fe8aaa80ae8367ebaf
Izaak Khaliq <(none)>
2016-06-19 17:33:13 +0800

    Add template for 'create', and remove index and show action in UserController because already available in 'resources' macro in Router

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,15 +2,15 @@ defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
   alias Rumbl.User
 
-  def index(conn, _params)  do
-    users = Repo.all(Rumbl.User)
-    render conn, "index.html", users: users
-  end
+  # def index(conn, _params)  do
+  #   users = Repo.all(Rumbl.User)
+  #   render conn, "index.html", users: users
+  # end
 
-  def show(conn, %{"id" => id}) do
-    user = Repo.get(Rumbl.User, id)
-    render conn, "show.html", user: user
-  end
+  # def show(conn, %{"id" => id}) do
+  #   user = Repo.get(Rumbl.User, id)
+  #   render conn, "show.html", user: user
+  # end
 
   def new(conn, _params) do
     changeset = User.changeset(%User{})
