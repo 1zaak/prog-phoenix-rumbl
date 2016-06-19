6ebbebd76545f06edc0a763f6a707ee7abbfaca3
Izaak Khaliq <izaak@izaak.my>
2016-06-19 17:18:57 +0800

    Declare new routes for new resources

--- web/router.ex
+++ web/router.ex
@@ -15,9 +15,10 @@ defmodule Rumbl.Router do
 
   scope "/", Rumbl do
     pipe_through :browser # Use the default browser stack
-    get "/users", UserController, :index
-    get "/users/:id", UserController, :show
+    # get "/users", UserController, :index
+    # get "/users/:id", UserController, :show
     get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
   end
 
   # Other scopes may use custom stacks.
