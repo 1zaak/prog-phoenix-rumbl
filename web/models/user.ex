236ed968c8ff331f6c8efbf6bf2590892dec2a7f
Izaak Khaliq <izaak@izaak.my>
2016-06-25 22:54:09 +0800

    Changeset to manage password

--- web/models/user.ex
+++ web/models/user.ex
@@ -16,5 +16,13 @@ defmodule Rumbl.User do
     |> validate_length(:username, min: 1, max: 20)
   end
 
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(password), [])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
+  end
+
   # defstruct [:id, :name, :username, :password]
 end
