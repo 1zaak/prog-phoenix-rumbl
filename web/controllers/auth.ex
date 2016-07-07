b3f4db7fb931ba589f0bf6674e67cda6d236586b
Izaak Khaliq <izaak@izaak.my>
2016-07-07 17:56:24 +0800

    Fix undefined reference to page_path

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -46,7 +46,7 @@ defmodule Rumbl.Auth do
     else
       conn
       |> put_flash(:error, "You must be logged in to access this page")
-      |> redirect(to: page_path(conn, :index))
+      |> redirect(to: Helpers.page_path(conn, :index))
       |> halt()
     end
   end
