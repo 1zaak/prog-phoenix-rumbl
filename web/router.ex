46ac56ba2d367c938db6cfe0068a25c2cb2724fe
Izaak Khaliq <izaak@izaak.my>
2016-07-07 17:54:48 +0800

    Move resources into a new scope in router

--- web/router.ex
+++ web/router.ex
@@ -21,6 +21,12 @@ defmodule Rumbl.Router do
     get "/", PageController, :index
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
+
+  end
+
+  scope "/manage", Rumbl do
+    pipe_through [:browser, :authenticate_user]
+
     resources "/videos", VideoController, only: [:new, :create, :delete]
   end
 
