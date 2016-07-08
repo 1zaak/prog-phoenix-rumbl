f18c7f7704e554d0a9c7d51cf4e678d0127a5ed8
Izaak Khaliq <izaak@izaak.my>
2016-07-09 01:46:09 +0800

    Use new 'user_videos' action in 'index' and 'show'

--- web/controllers/video_controller.ex
+++ web/controllers/video_controller.ex
@@ -10,8 +10,8 @@ defmodule Rumbl.VideoController do
           [conn, conn.params, conn.assigns.current_user])
   end
 
-  def index(conn, _params) do
-    videos = Repo.all(Video)
+  def index(conn, _params, user) do
+    videos = Repo.all(user_videos(user))
     render(conn, "index.html", videos: videos)
   end
 
@@ -38,8 +38,8 @@ defmodule Rumbl.VideoController do
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    video = Repo.get!(Video, id)
+  def show(conn, %{"id" => id}, user) do
+    video = Repo.get!(user_videos(user), id)
     render(conn, "show.html", video: video)
   end
 
