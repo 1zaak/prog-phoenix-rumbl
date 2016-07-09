3f984e9f43d26abf1c39e97bb124b7f5543e7466
Izaak Khaliq <izaak@izaak.my>
2016-07-10 00:01:08 +0800

    Fix deleted 'categories' table in migrations

--- priv/repo/migrations/20160709153729_create_category.exs
+++ priv/repo/migrations/20160709153729_create_category.exs
@@ -2,8 +2,16 @@ defmodule Rumbl.Repo.Migrations.CreateCategory do
   use Ecto.Migration
 
   def change do
+    create table(:categories) do
+      add :name, :string, null: false
+
+      timestamps
+    end
+
     alter table(:videos) do
       add :category_id, references(:categories)
     end
+
+    create_unique_index(:categories, [:name])
   end
 end
