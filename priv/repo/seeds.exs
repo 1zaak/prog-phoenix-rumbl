023d0300c8fd2579ba0ac10d636e6fbf093d3707
Izaak Khaliq <izaak@izaak.my>
2016-07-10 11:57:53 +0800

    Simple constraint to check for existing name in category table

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -13,5 +13,6 @@ alias Rumbl.Repo
 alias Rumbl.Category
 
 for category <- ~w(Action Drama Romance Comedy Sci-fi) do
+  Repo.get_by(Category, name: category) ||
   Repo.insert!(%Category{name: category})
 end
