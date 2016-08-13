2121cd2b8cac98cfeccc0914326c18b07d9528ea
Izaak Khaliq <izaak@izaak.my>
2016-08-13 20:01:42 +0800

    Change conn to conn_case based on Phoenix latest changelogs

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -38,6 +38,6 @@ defmodule Rumbl.ConnCase do
       Ecto.Adapters.SQL.restart_test_transaction(Rumbl.Repo, [])
     end
 
-    {:ok, conn: Phoenix.ConnTest.conn()}
+    {:ok, conn: Phoenix.ConnTest.conn_case()}
   end
 end
