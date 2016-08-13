367838d9949e816f8f90bf6594c24ac6495f0493
Izaak Khaliq <izaak@izaak.my>
2016-08-13 20:53:40 +0800

    Test for listing all user's video on index, and bypass authentication which makes auth tests fail

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -1,6 +1,22 @@
 defmodule Rumbl.VideoControllerTest do
   use Rumbl.ConnCase
 
+  setup do
+    user = insert_user(username: "max")
+    conn = assign(conn(), :current_user, user)
+    {:ok, conn: conn, user: user}
+  end
+
+  test "list all user's video on index", %{conn: conn, user: user} do
+    user_video = insert_video(user, title: "funny cats")
+    other_video = insert_video(insert_user(username: "other"), title: "another video")
+
+    conn = get conn, video_path(conn, :index)
+    assert html_response(conn, 200) =~ ~r/Listing videos/
+    assert String.contains?(conn.resp_body, user_video.title)
+    refute String.contains?(conn.resp_body, other_video.title)
+  end
+
   test "requires user authentication on all actions", %{conn: conn} do
     Enum.each([
       get(conn, video_path(conn, :new)),
